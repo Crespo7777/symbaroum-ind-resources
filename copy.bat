3ee769f333c69851c70ff50f2b796d2571c9ef69
Crespo7777 <projettosrpg@gmail.com>
2026-06-22 20:24:26 -0300

    Delete copy.bat

--- copy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Sincronizando arquivos do modulo Symbaroum Ind Resources...
-robocopy "C:\Projetos\Symbaroum Resources\symbaroum-ind-resources" "C:\Users\heito\Documents\FoundryVTT\Data\modules\symbaroum-ind-resources" /MIR /XD .git
-echo Sincronizacao concluida!
-pause
